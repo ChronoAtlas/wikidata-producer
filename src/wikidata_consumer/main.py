b3e795ac8ba5d48027b8587c5947321afc5fd1f8
Andreas <andreas.kruehlmann@siemens.com>
2024-02-13 14:04:30 +0100

    Add demo test

--- src/wikidata_consumer/main.py
+++ src/wikidata_consumer/main.py
@@ -1,13 +1,16 @@
-import typer
 import time
+
+import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def run() -> None:
-    while True:
+    while True:  # noqa: WPS457
         print("Hello, World!")
         time.sleep(3)
 
+
 if __name__ == "__main__":
-    app();
+    app()
